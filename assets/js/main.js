1f96599737b3979ecef5afdd1738991518b384c8
Alebiosu Opeyemi <alebiosuopeyemi18@gmail.com>
2026-04-24 18:25:09 +0100

    Update main.js

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+<script>
+  const toggle = document.getElementById('nav-toggle');
+  const navLinks = document.querySelector('.nav-links');
+  toggle.addEventListener('click', () => {
+  navLinks.classList.toggle('open');
+  });
+</script>
